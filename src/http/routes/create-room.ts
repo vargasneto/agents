0e15b771ebd825fdf5d73ef9bd7e66d3df00d288
Pedro <pedrohjs_vargas@hotmail.com>
2025-07-13 00:49:22 -0300

    feat: rotas criadas /questions/rooms

---
+++ src/http/routes/create-room.ts
@@ -0,0 +1,36 @@
+import type { FastifyPluginCallbackZod } from "fastify-type-provider-zod";
+import { z } from "zod/v4";
+import { db } from "../../db/connection.ts";
+import { schema } from "../../db/schema/index.ts";
+
+export const createRoomRoute: FastifyPluginCallbackZod = (app) => {
+  app.post(
+    "/rooms",
+    {
+      schema: {
+        body: z.object({
+          name: z.string().min(1),
+          description: z.string().optional(),
+        }),
+      },
+    },
+    async (request, reply) => {
+      const { name, description } = request.body;
+
+      const result = await db
+        .insert(schema.rooms)
+        .values({
+          name,
+          description,
+        })
+        .returning();
+
+      const insertedRoom = result[0];
+
+      if (!result[0]) {
+        throw new Error("FALHA PARA CRIAR UMA NOVA SALA");
+      }
+      return reply.status(201).send({ roomId: insertedRoom.id });
+    }
+  );
+};
--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,9 @@ import {
 } from "fastify-type-provider-zod";
 import { env } from "./env.ts";
 import { getRoomsRoute } from "./http/routes/get-rooms.ts";
+import { createRoomRoute } from "./http/routes/create-room.ts";
+import { getRoomsQuestions } from "./http/routes/get-room-questions.ts";
+import { createQuestionRoute } from "./http/routes/create-question.ts";
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
 app.register(fastifyCors, { origin: "http://localhost:5173" });
@@ -19,5 +22,8 @@ app.get("/health", () => {
 });
 
 app.register(getRoomsRoute);
+app.register(createRoomRoute);
+app.register(getRoomsQuestions);
+app.register(createQuestionRoute);
 
 app.listen({ port: env.PORT });
